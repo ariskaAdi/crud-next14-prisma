56da4c7c4044d09b4da4e58e21fa069ef43ffb8c
ariskaAdi <ariskaadiprasetyo@gmail.com>
2024-05-01 19:12:14 +0700

    add new data using zod

--- components/create-form.tsx
+++ components/create-form.tsx
@@ -1,18 +1,34 @@
+"use client";
+
+import { SaveContact } from "@/lib/actions";
+import { useFormState } from "react-dom";
+
 const CreateForm = () => {
+  const [state, formAction] = useFormState(SaveContact, null);
+
   return (
     <div>
-      <form action="">
+      <form action={formAction}>
         <div className="mb-5">
           <label htmlFor="name" className="dis-block text-sm font-medium text-gray-900">
             Full Name
           </label>
           <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Full Name..." />
         </div>
+        <div id="name-error" aria-live="polite" aria-atomic="true">
+          <p className="mt-2 text-sm text-red-500">{state?.Error?.name}</p>
+        </div>
         <div className="mb-5">
           <label htmlFor="phone" className="dis-block text-sm font-medium text-gray-900">
-            Full Name
+            Phone Number
           </label>
           <input type="text" name="phone" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="phone..." />
+          <div id="phone-error" aria-live="polite" aria-atomic="true">
+            <p className="mt-2 text-sm text-red-500">{state?.Error?.phone}</p>
+          </div>
+        </div>
+        <div id="message-error" aria-live="polite" aria-atomic="true">
+          <p className="mt-2 text-sm text-red-500">{state?.message}</p>
         </div>
         <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-sm text-sm w-full px-5 py-3 text-center">
           Save
