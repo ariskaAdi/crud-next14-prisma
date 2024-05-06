4eeb76de0d81c5e1b3b5a14ddec60e81b1536b8a
ariskaAdi <ariskaadiprasetyo@gmail.com>
2024-05-06 20:48:35 +0700

    update data contact work properly

--- components/edit-form.tsx
+++ components/edit-form.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { SaveContact } from "@/lib/actions";
+import { updateContact } from "@/lib/actions";
 import { useFormState } from "react-dom";
 import { SubmitButton } from "./buttons";
 import type { Contact } from "@prisma/client";
 
 const UpdateForm = ({ contact }: { contact: Contact }) => {
-  const [state, formAction] = useFormState(SaveContact, null);
+  const UpdateContactById = updateContact.bind(null, contact.id);
+  const [state, formAction] = useFormState(UpdateContactById, null);
 
   return (
     <div>
